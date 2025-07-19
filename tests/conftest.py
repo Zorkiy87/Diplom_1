6e9872dcf7055bf68909de68a06c9a80a7297d96
Mikhail Kozulin <zorkiy87@yandex.ru>
2025-07-19 13:06:24 +0300

    1) Убраны фикстуры burger и database. Переписаны тесты на которые они влияли. 2) Изменён тест test_get_receipt, убрана проверка через in 3) Разделены тесты ингредиентов, сейчас отдельные: test_ingredient_init_type,  test_ingredient_init_name, test_ingredient_init_price

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,6 @@ from unittest.mock import Mock
 
 import pytest
 
-from praktikum.burger import Burger
-from praktikum.database import Database
-
-
-@pytest.fixture()
-def burger():
-    return Burger()
 
 @pytest.fixture()
 def bun_mock():
@@ -24,6 +17,7 @@ def ingredient_mock():
     ingredient_mock.get_name.return_value = 'test sause'
     ingredient_mock.get_price.return_value = 22.0
     return ingredient_mock
+
 @pytest.fixture()
 def other_ingredient_mock():
     other_ingredient_mock = Mock()
@@ -31,9 +25,5 @@ def other_ingredient_mock():
     other_ingredient_mock.get_name.return_value = 'test filling'
     other_ingredient_mock.get_price.return_value = 42.0
     return other_ingredient_mock
-
-@pytest.fixture()
-def database():
-    return Database()
 
 
--- tests/test_ingridient.py
+++ tests/test_ingridient.py
@@ -2,12 +2,27 @@ import pytest
 from praktikum.ingredient import Ingredient
 from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 
+
 @pytest.mark.parametrize('ingredient_type, name, price', [
     (INGREDIENT_TYPE_FILLING, 'big naggets', 250.20),
     (INGREDIENT_TYPE_SAUCE, 'garlic sauce', 95.00)
 ])
-def test_igredient_init(ingredient_type,name, price):
+def test_ingredient_init_type(ingredient_type,name, price):
     ing = Ingredient(ingredient_type,name,price)
     assert ing.get_type() == ingredient_type
+
+@pytest.mark.parametrize('ingredient_type, name, price', [
+    (INGREDIENT_TYPE_FILLING, 'big naggets', 250.20),
+    (INGREDIENT_TYPE_SAUCE, 'garlic sauce', 95.00)
+])
+def test_ingredient_init_name(ingredient_type,name, price):
+    ing = Ingredient(ingredient_type,name,price)
     assert ing.get_name() == name
+
+@pytest.mark.parametrize('ingredient_type, name, price', [
+    (INGREDIENT_TYPE_FILLING, 'big naggets', 250.20),
+    (INGREDIENT_TYPE_SAUCE, 'garlic sauce', 95.00)
+])
+def test_ingredient_init_price(ingredient_type,name, price):
+    ing = Ingredient(ingredient_type,name,price)
     assert ing.get_price() == price
