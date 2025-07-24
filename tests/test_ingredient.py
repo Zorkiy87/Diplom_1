3f7e6024ba734f41fea3e5c6a7075e561c55b6e9
Mikhail Kozulin <zorkiy87@yandex.ru>
2025-07-24 20:47:40 +0300

    1) Тесты из test_ingredient объединены в тестовый класс TestIngredient

---
+++ tests/test_ingredient.py
@@ -0,0 +1,29 @@
+import pytest
+from praktikum.ingredient import Ingredient
+from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
+
+
+class TestIngredient:
+    @pytest.mark.parametrize('ingredient_type, name, price', [
+        (INGREDIENT_TYPE_FILLING, 'big naggets', 250.20),
+        (INGREDIENT_TYPE_SAUCE, 'garlic sauce', 95.00)
+    ])
+    def test_ingredient_init_type(self, ingredient_type,name, price):
+        ing = Ingredient(ingredient_type,name,price)
+        assert ing.get_type() == ingredient_type
+
+    @pytest.mark.parametrize('ingredient_type, name, price', [
+        (INGREDIENT_TYPE_FILLING, 'big naggets', 250.20),
+        (INGREDIENT_TYPE_SAUCE, 'garlic sauce', 95.00)
+    ])
+    def test_ingredient_init_name(self, ingredient_type,name, price):
+        ing = Ingredient(ingredient_type,name,price)
+        assert ing.get_name() == name
+
+    @pytest.mark.parametrize('ingredient_type, name, price', [
+        (INGREDIENT_TYPE_FILLING, 'big naggets', 250.20),
+        (INGREDIENT_TYPE_SAUCE, 'garlic sauce', 95.00)
+    ])
+    def test_ingredient_init_price(self, ingredient_type,name, price):
+        ing = Ingredient(ingredient_type,name,price)
+        assert ing.get_price() == price
